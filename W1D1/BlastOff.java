742d96b672a3688436b7ef0d8f17038df617f56e
Michael Zijlstra <mzijlstra@mum.edu>
2015-02-23 11:40:57 -0600

    Some more example I made during W1D1

---
+++ W1D1/BlastOff.java
@@ -0,0 +1,10 @@
+public class BlastOff {
+	public static void main(String[] args) {
+		int num = 5;
+		while (num != 0) {
+			System.out.println(num);
+			num = num -1;
+		}
+		System.out.println("Blast Off!");
+	}
+}
--- W1D1/HelloWorld.java
+++ W1D1/HelloWorld.java
@@ -1,5 +1,23 @@
+// This is a class declaration, each file has to have one class
 public class HelloWorld {
+
+	// This is the main method, the starting point of the app
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+		// This is my one statement telling the computer do things
+		System.out.println("Hello World");
+
+		long five; // declaration of a variable
+		five = getFive(); // assignment, from a method call
+		System.out.println(five);
+
+		/*
+		 *  This is a fancy multi-line comment
+		 *  Look, this is the second line
+		 */
+	}
+
+	public static int getFive() {
+		return 5;
 	}
 }
