6b259b31eca261ed16e58ddf7de58f35c44e031c
Michael Zijlstra <mzijlstra@mum.edu>
2015-02-23 11:51:56 -0600

    Switched to for loop

--- W1D1/BlastOff.java
+++ W1D1/BlastOff.java
@@ -1,9 +1,12 @@
 public class BlastOff {
 	public static void main(String[] args) {
-		int num = 5;
-		while (num != 0) {
+		// initialization
+		
+		// condition on variable
+		for (int num = 5; num != 0; num = num -1) {
 			System.out.println(num);
-			num = num -1;
+			// change to variable
+			
 		}
 		System.out.println("Blast Off!");
 	}
